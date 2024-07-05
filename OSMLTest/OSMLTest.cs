59f9a4c785cc9894a85aaaa1002d3a03fb5197ce
BoettcherDasOriginal <boettcherleonard@gmail.com>
2024-07-05 13:29:40 +0200

    unstable shop tests

--- OSMLTest/OSMLTest.cs
+++ OSMLTest/OSMLTest.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Reflection;
 using OSLoader;
 using OSML;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OSMLTest
 {
@@ -27,6 +32,38 @@ namespace OSMLTest
             {
                 GameObject ft = new GameObject("FurnitureTest", typeof(FurnitureTest));
             }
+
+            /*
+            if(scene.buildIndex == 11)
+            {
+                var shop = GameObject.FindObjectOfType<FurnitureShop>();
+                if (shop != null)
+                {
+                    string path = Path.Combine(Assembly.GetExecutingAssembly().Location.Substring(0, Assembly.GetExecutingAssembly().Location.Length - 13), "osml_box.json");
+
+                    if (File.Exists(path))
+                    {
+                        try
+                        {
+                            string rawFurnitureConfig = File.ReadAllText(path);
+
+                            FurnitureConfig? furnitureConfig = JsonConvert.DeserializeObject<FurnitureConfig>(rawFurnitureConfig);
+                            if (furnitureConfig != null)
+                            {
+                                furnitureConfig.assetBundlePath = path.Substring(0, path.Length - Path.GetFileName(path).Length) + furnitureConfig.assetBundlePath;
+                                Furniture f = FurnitureCreator.FurnitureConfigToFurniture(furnitureConfig);
+                                f.addressableAssetPath = $"OSML_Furniture<#>{path}";
+
+                                shop.AddFurniture(f, 3);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogError(e);
+                        }
+                    }
+                }
+            }*/
         }
     }
 }
